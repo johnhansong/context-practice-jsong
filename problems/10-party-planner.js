067ab8773f49cab200f3e31ccb3af45571eaac2d
hazelcaling <hazelcaling@live.com>
2023-06-19 22:01:40 -0700

    07 to 09 done, 10 in progress

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -1,4 +1,28 @@
-// Your code here
+class PartyPlanner {
+	constructor(guestList) {
+		this.guestList = [];
+	}
+
+	addToGuestList(name) {
+		this.guestList.push(name)
+
+	}
+
+	throwParty() {
+		const name1 = this.guestList[0];
+		if (!this.guestList.length) {
+			console.log("Gotta add people to the guest list")
+		} else if (this.guestList.length === 1) {
+
+			console.log(`Welcome to the party ${name1}`)
+		} else {
+			const modArr = this.guestList.slice(1)
+			modArr.reduce((acc) => acc)
+			console.log(`Welcome to the party ${name1}`)
+		}
+	}
+
+}
 
 
 /*****************************************************************************/
