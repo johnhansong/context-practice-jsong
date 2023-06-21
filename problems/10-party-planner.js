b35c0f7e35c211b79f582ec467260271ec34b2d1
hazelcaling <hazelcaling@live.com>
2023-06-20 19:34:17 -0700

    10 and 11 solved

--- problems/10-party-planner.js
+++ problems/10-party-planner.js
@@ -11,19 +11,28 @@ class PartyPlanner {
 	throwParty() {
 		const name1 = this.guestList[0];
 		if (!this.guestList.length) {
-			console.log("Gotta add people to the guest list")
+			return "Gotta add people to the guest list"
 		} else if (this.guestList.length === 1) {
 
-			console.log(`Welcome to the party ${name1}`)
+			return `Welcome to the party ${name1}`
 		} else {
 			const modArr = this.guestList.slice(1)
-			modArr.reduce((acc) => acc)
-			console.log(`Welcome to the party ${name1}`)
+			const newStr = modArr.reduce((acc, el) => acc + `and ${el}`, `Welcome to the party ${name1} ` )
+			return newStr
 		}
 	}
-
 }
 
+
+const party = new PartyPlanner();
+
+console.log(party.throwParty()); // prints "Gotta add people to the guest list"
+
+party.addToGuestList("James");
+console.log(party.throwParty()); // prints "Welcome to the party James"
+
+party.addToGuestList("Alvin");
+console.log(party.throwParty()); // prints "Welcome to the party James and Alvin"
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
